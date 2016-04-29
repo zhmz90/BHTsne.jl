928646e70d3d72518e396e7085fe017679b0de36
zhmz90 <zhmz90@gmail.com>
2016-04-29 13:19:18 +0800

    add a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using BHtsne
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+samples = [1.0 0.0; 0.0 1.0]
+result = bh_tsne(samples, no_dims=2, perplexity=0.1)
+@test result == [-2458.83181442 -6525.87718385; 2458.83181442 6525.87718385]
