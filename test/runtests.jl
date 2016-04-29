7b7b86ad19fce46d53949fcb84f8ce6ff06289c6
zhmz90 <zhmz90@gmail.com>
2016-04-29 14:02:09 +0800

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using BHTsne
 using Base.Test
 
 samples = [1.0 0.0; 0.0 1.0]
-result = bh_tsne(samples, no_dims=2, perplexity=0.1)
-@test result == [-2458.83181442 -6525.87718385; 2458.83181442 6525.87718385]
+result = bh_tsne(samples, no_dims=2, perplexity=0.1, randseed=2016)
+@test_approx_eq result [-2070.3067044 6659.34094976; 2070.3067044 -6659.34094976]
